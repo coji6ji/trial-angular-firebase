4bd985af94d48031fa413ea40245caddd7b7eb79
coji6ji <koji.info.mail@gmail.com>
2019-12-21 17:50:32 +0900

    feat(exp-pipe): 経験値パイプ表示追加

---
+++ git-pet/src/app/exp.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Pet } from './interface/pet';
+
+const expTable = [
+  20,
+  40,
+  100,
+  500,
+  1000,
+  3000,
+  5000
+];
+
+@Pipe({
+  name: 'exp'
+})
+
+export class ExpPipe implements PipeTransform {
+
+  transform(pet: Pet, ...args: any[]): any {
+    const totalExp = pet.exp;
+    const level = pet.level;
+    const baseExp = expTable[level - 2] || 0;
+    const nextExp = expTable[level - 1] - baseExp;
+    const exp = totalExp - baseExp;
+    return exp + '/' + nextExp;
+  }
+
+}
